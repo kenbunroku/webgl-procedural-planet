b7849f1c4be45828e40f086fcf30adc956abcb53
kenbunroku <kenbunroku524@gmail.com>
2024-02-09 07:33:44 -0600

    create utils

--- src/scripts/libs/webglUtils.js
+++ src/scripts/libs/webglUtils.js
@@ -67,3 +67,11 @@ export const createVbo = (gl, data, usage) => {
   gl.bindBuffer(gl.ARRAY_BUFFER, null);
   return vbo;
 };
+
+export const createIbo = (gl, data) => {
+  const ibo = gl.createBuffer();
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, ibo);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(data), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+  return ibo;
+};
